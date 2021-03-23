09865f34df82934bc970949b2d00d99a29fa71d0
Alex <alex.zzh.s7@gmail.com>
2021-03-23 16:23:33 +0000

    Add hash for font files

--- _utils/font.js
+++ _utils/font.js
@@ -2,6 +2,8 @@ const fg = require('fast-glob')
 const fs = require('fs-extra')
 const matter = require('gray-matter')
 const util = require('util')
+const md5 = require('md5')
+const Path = require('path')
 const exec = util.promisify(require('child_process').exec)
 
 let zh = '',
@@ -13,6 +15,8 @@ for (let i = 33; i <= 126; i++)
 const basic = String.fromCharCode.apply(this, range) + String.fromCharCode(11834) + '一二三四五六七八九十'
 
 const loadChar = async () => {
+  await Promise.all([exec('rm assets/SourceHanSerifCN/*'), exec('rm assets/SourceHanSerifJP/*')])
+  
   const entries = await fg('!(node_modules)/**/*.md')
   for (const entry of entries) {
     const content = await fs.readFile(entry, 'utf-8')
@@ -65,7 +69,50 @@ const loadChar = async () => {
     )
     .flat()
 
-  Promise.all(commands)
+  await Promise.all(commands)
 }
 
-loadChar()
+const parsePath = path => {
+  const extname = Path.extname(path)
+  return {
+    dirname: Path.dirname(path),
+    basename: Path.basename(path, extname),
+    extname: extname
+  }
+}
+
+const addHash = async () => {
+  let map = []
+  const fonts = await fg('assets/**/*.{woff,woff2}')
+  for (const entry of fonts) {
+    const content = await fs.readFile(entry, 'utf-8')
+    let parsedPath = parsePath(entry)
+    
+    if (parsedPath.basename.split('.').length > 1)
+      parsedPath.basename = parsedPath.basename.split('.')[0]
+
+    const hash = '.' + md5(content)
+    const path = Path.join(parsedPath.dirname, parsedPath.basename + hash + parsedPath.extname)
+
+    fs.rename(entry, path)
+    map.push([parsedPath.basename + parsedPath.extname, parsedPath.basename + hash + parsedPath.extname])
+  }
+  
+  // make sure xx.woff2 appears before xx.woff so that when replacing .woff2 won't be replaced by .woff
+  map.sort((a, b) => b[0].localeCompare(a[0]))
+  
+  const files = ['_includes/components/preload.njk', '_includes/css/_fonts.scss']
+  files.forEach(async file => {
+    let content = await fs.readFile(file, 'utf-8')
+    for (const key of map) {
+      const reg = new RegExp(key[0], 'g')
+      content = content.replace(reg, key[1])
+    }
+    fs.writeFile(file, content)
+  })
+}
+
+loadChar().then(() => {
+  addHash()
+})
+
