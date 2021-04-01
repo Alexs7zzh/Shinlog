936895e8ccc09ec71a578c07b1ea296e1463085f
Alex <alex.zzh.s7@gmail.com>
2021-04-01 06:52:55 +0000

    Embed js in head

--- _includes/js/head.js
+++
@@ -1,4 +0,0 @@
-document.documentElement.style.setProperty('--width', window.innerWidth)
-window.addEventListener('resize', () => {
-  document.documentElement.style.setProperty('--width', window.innerWidth)
-})
