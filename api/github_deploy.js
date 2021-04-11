40cde1a7f3432f56b17ca5b60b4f2d895d57272c
Alex <alex.zzh.s7@gmail.com>
2021-04-11 05:08:45 +0000

    Update github webhook

--- api/github_deploy.js
+++ api/github_deploy.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 /* global process */
-const telegram = async (content, notification = false) =>
+const telegram = async (content, notification = false, me = true) =>
   axios.post(`https://api.telegram.org/bot${process.env.telegram_token}/sendMessage`, {
-    chat_id: process.env.telegram_chat_id,
+    chat_id: me ? process.env.telegram_chat_id : process.env.telegram_chat_id_jesse,
     text: content,
     disable_notification: notification,
     parse_mode: 'markdown',
@@ -16,16 +16,20 @@ const capitalize = s => {
 
 module.exports = (req, res) => {
   const status = req.body.deployment_status
-  if (status.state === 'pending') {
-    res.status(200).send()
-    return
-  }
+  if (status.state === 'pending')
+    return res.status(200).send()
+  
+  let url = ''
+  if (status.target_url.includes('shinlog')) url = 'https://shinlog.me'
+  if (status.target_url.includes('charlsy')) url = 'https://charlsy.me'
+  if (status.target_url.includes('jesse'))   url = 'https://jesor.me'
   
   const notification = status.state === 'success'
   const content = `*${capitalize(status.state)}*  
 ${status.description}.  
-${status.target_url.includes('shinlog') ? 'https://shinlog.me' : 'https://charlsy.me'}`
+${url}`
   
+  telegram(content, notification, false)
   telegram(content, notification)
     .then(() => {
       res.status(200).send()
