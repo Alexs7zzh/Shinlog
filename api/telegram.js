56706a6a279ba751ac39e5b94464f53005fe1394
Alex <alex.zzh.s7@gmail.com>
2021-04-19 15:43:20 +0000

    Switch from axios to node-fetch

--- api/telegram.js
+++ api/telegram.js
@@ -1,12 +1,16 @@
-const axios = require('axios')
+const fetch = require('node-fetch')
 /* global process */
 const telegram = async (content, notification = false) =>
-  axios.post(`https://api.telegram.org/bot${process.env.telegram_token}/sendMessage`, {
-    chat_id: process.env.telegram_chat_id,
-    text: content,
-    disable_notification: notification,
-    parse_mode: 'markdown',
-    disable_web_page_preview: true
+  fetch(`https://api.telegram.org/bot${process.env.telegram_token}/sendMessage`, {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      chat_id: process.env.telegram_chat_id,
+      text: content,
+      disable_notification: notification,
+      parse_mode: 'markdown',
+      disable_web_page_preview: true
+    })
   })
 
 module.exports = (req, res) => {
