56b2b2997e95443af61469ea511380e2d5b55485
Alex Zhou <alex.zzh.s7@gmail.com>
2022-02-05 17:56:03 +0900

    Rewrite scss compile

--- src/utils/plugins/sass.js
+++ src/utils/plugins/sass.js
@@ -1,4 +1,3 @@
-const Mutex = require('async-mutex').Mutex
 const md5 = require('md5')
 const { basename, extname } = require('path')
 const fs = require('fs')
@@ -8,90 +7,52 @@ const csso = require('csso')
 const autoprefixer = require('autoprefixer')
 const postcss = require('postcss')
 
+let result = {}
+
 const compile = async () => {
   const files = await fg('src/scss/[!_]*.{scss,sass}')
-  
+
   for (const entry of files) {
     const ext = extname(entry)
     const name = basename(entry, ext)
     
-    const { css } = sass.renderSync({
-      file: entry
-    })
+    let { css } = sass.compile(entry)
+
+    if (process.env.PRODUCTION) {
+      css = postcss([autoprefixer]).process(css).css
+      css = csso.minify(css, {
+        sourceMap: false,
+        restructure: true,
+        forceMediaMerge: false
+      }).css
+      const hash = md5(css).slice(0, 10)
+      result[name + '.css'] = `${name}-${hash}.css`
+    } else
+      result[name + '.css'] = `${name}.css`
     
     try {
       fs.mkdirSync('_site/')
     }  catch (err) {
       'nothing'
     }
-    fs.writeFileSync(`_site/${name}.css`, css) 
+    fs.writeFileSync(`_site/${result[name + '.css']}`, css) 
   }
-}
-
-const compileWatch = changed => {
-  if (changed === undefined) return
-  
-  changed = changed.filter(i => i.endsWith('scss') || i.endsWith('sass'))
-  
-  if(changed.length > 0)
-    compile()
-}
-
-const build = async () => {
-  const files = await fg('src/scss/[!_]*.{scss,sass}')
-  let result = {}
-  
-  for (const entry of files) {
-    const ext = extname(entry)
-    const name = basename(entry, ext)
-    
-    let { css } = sass.renderSync({
-      file: entry
-    })
-    
-    css = postcss([autoprefixer]).process(css).css
-    
-    css = csso.minify(css, {
-      sourceMap: false,
-      restructure: true,
-      forceMediaMerge: false
-    }).css
-    
-    const hash = md5(css).slice(0, 10)
-    result[name + '.css'] = `${name}-${hash}.css`
-    
-    try {
-      fs.mkdirSync('_site/')
-    }  catch (er) {
-      'nothing'
-    }
-    fs.writeFileSync(`_site/${name}-${hash}.css`, css) 
-  }
-
-  return result
 }
 
 module.exports = config => {
-  const mutex = new Mutex()
-  let cssHash = {}
-  /* global process */
+  config.on('beforeBuild', compile)
+
   if (!process.env.PRODUCTION) {
     config.addWatchTarget('./src/scss/')
-    config.on('beforeBuild', compile)
-    config.on('beforeWatch', compileWatch)
-    config.addFilter('cssHash', url => url)
-  } else
-    config.addNunjucksAsyncFilter('cssHash', async (url, callback) => {
-      const release = await mutex.acquire()
-      try {
-        if (Object.keys(cssHash).length === 0)
-          cssHash = await build()
-      } finally {
-        release()
-      }
-      for(let i in cssHash)
-        url = url.replace(i, cssHash[i])
-      callback(null, url)
+    config.on('beforeWatch', changed => {
+      if (changed === undefined) return
+      changed = changed.filter(i => i.endsWith('scss') || i.endsWith('sass'))
+      if(changed.length > 0) compile()
     })
+  }
 
+  config.addFilter('cssHash', filename => {
+    if (result[filename]) return result[filename]
+    else return filename
+  })
 }
